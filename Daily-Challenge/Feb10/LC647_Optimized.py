a58c5678f7ea3e9009defea6abfddb9f424e5d62
ANIKET PATEL <62117194+aniketpatel12@users.noreply.github.com>
2024-02-10 17:29:05 +0000

    Added Optimized solution of LC647 Palindromic Substring problem

---
+++ Daily-Challenge/Feb10/LC647_Optimized.py
@@ -0,0 +1,42 @@
+'''
+Given a string s, return the number of palindromic substrings in it.
+
+A string is a palindrome when it reads the same backward as forward.
+
+A substring is a contiguous sequence of characters within the string.
+
+Example 1:
+
+Input: s = "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+Example 2:
+
+Input: s = "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+
+'''
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        ans = 0
+
+        def countPalindrome(s, l, r):
+            res = 0
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                res += 1
+                l -= 1
+                r += 1
+            return res
+
+        for i in range(len(s)):
+            # odd-length substrings
+            l = r = i
+            ans += countPalindrome(s, l, r)
+
+             # even-length substrings
+            l = i
+            r = i + 1
+            ans += countPalindrome(s, l, r)
+
+        return ans
